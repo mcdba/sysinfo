bb39325524690746dd123d1dac29c372dbc19a24
Michail Kabanov <mcdba@mail.ru>
2016-12-20 13:03:37 +0300

    Fix netstat

--- addsudousers.sh
+++ addsudousers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while [[ -n $1 ]]; do
-    echo "$1	ALL=(ALL) NOPASSWD: /usr/sbin/tcpdump" >> /etc/sudoers;
+    echo "$1	ALL=(ALL) NOPASSWD: /usr/sbin/tcpdump,/bin/netstat" >> /etc/sudoers;
     shift # shift all parameters;
 done
